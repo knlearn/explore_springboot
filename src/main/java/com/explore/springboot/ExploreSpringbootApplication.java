1278d4799e1543f8718a9f5e1d015efddad37673
knlearndev <knlearndev@gmail.com>
2024-03-21 22:26:15 +0530

    Initial commit

---
+++ src/main/java/com/explore/springboot/ExploreSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.explore.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreSpringbootApplication.class, args);
+	}
+
+}
